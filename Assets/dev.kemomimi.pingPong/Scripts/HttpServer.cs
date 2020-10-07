59d7261e5ee936e8fcb3c79694610c3c2374a7a4
karasawa <mikki.stad@gmail.com>
2020-10-08 00:03:13 +0900

    Initialize commit

---
+++ Assets/dev.kemomimi.pingPong/Scripts/HttpServer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace dev.kemomimi.m5pingpong
+{
+    [Serializable]
+    public class OnRequestEvent : UnityEvent<HttpListenerContext> { }
+}
+
+namespace dev.kemomimi.m5pingpong
+{
+    public class HttpServer : MonoBehaviour
+    {
+        private HttpListener httpListener = new HttpListener();
+
+        public int port = 80;
+        public string path = "/";
+
+        public OnRequestEvent OnRequest;
+
+        void Start()
+        {
+            httpListener.Prefixes.Add("http://*:" + port + path);
+            _ = StartServer();
+        }
+
+        public async Task StartServer()
+        {
+            httpListener.Start();
+
+            while (true)
+            {
+                var context = await httpListener.GetContextAsync();
+
+                OnRequest.Invoke(context);
+            }
+        }
+
+        void OnDestroy()
+        {
+            httpListener.Stop();
+        }
+    }
+}
---
+++ Assets/dev.kemomimi.pingPong/Scripts/PingPongReceiver.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using UnityEngine;
+
+namespace dev.kemomimi.m5pingpong
+{
+    public class PingPongReceiver : MonoBehaviour
+    {
+        [SerializeField] private string message = "Success";
+
+        [Space, SerializeField] private Animator pingPongAnimator;
+        [SerializeField] private int ch;
+
+        public void OnRequest(HttpListenerContext context)
+        {
+            var data = System.Text.Encoding.UTF8.GetBytes(message);
+            context.Response.StatusCode = 200;
+            context.Response.Close(data, false);
+
+            var str = context.Request.RawUrl;
+            str = str.Substring(1, str.Length - 1);
+            //Debug.Log(str);
+
+            if (ch == int.Parse(str))
+            {
+                OpenPingPong();
+            }
+        }
+
+        public void OpenPingPong()
+        {
+            pingPongAnimator.SetBool("Open", true);
+        }
+
+        public void ClosePingPong()
+        {
+            pingPongAnimator.SetBool("Open", false);
+        }
+    }
+
+}
